e35d50d9fefbb4acc193fa62a25b2bebc25724b2
jaci-hub <jacintoquimua7@gmail.com>
2020-08-08 00:30:43 -0600

    Now it works!

--- alphabeorder.cpp
+++ alphabeorder.cpp
@@ -7,30 +7,32 @@ string list[5];
 
 void ordering()
 {
-    for(int i=0; i<5; i++){
-    cin>>list[i];
-    for(int m=0; m<5; m++){
-        for(int j=m+1; j<5; j++){
-            if(list[m]>list[j]){
-                a=list[m];
-                list[m]=list[j];
-                list[j]=a;
+    for (int i = 0; i < 5; i++)
+        cin >> list[i];
+    for (int m = 0; m < 5; m++)
+    {
+        for (int j = m + 1; j < 5; j++)
+        {
+            if (list[m] > list[j])
+            {
+                a = list[m];
+                list[m] = list[j];
+                list[j] = a;
             }
         }
-    }
     }
 }
 
 int main()
 {
-    cout<<"Enter 5 words: ";
+    cout << "Enter 5 words: ";
     ordering();
     cout << "Alphebetical order: " << endl;
-    for (int k = 0; k < 5; k++){
+    for (int k = 0; k < 5; k++)
+    {
         cout << list[k] << " ";
     }
     cout << endl;
 
     return 0;
 }
-
