890a7faf6d51862406c0e2c71f7b5f8dbfd1cdeb
jaci-hub <jacintoquimua7@gmail.com>
2020-08-08 01:07:22 -0600

    just improved this code!

--- removespace.cpp
+++ removespace.cpp
@@ -5,14 +5,21 @@ using namespace std;
 int main()
 {
     string name;
-    cout << "Enter name: ";
+    int count = 1;
+    cout << "Enter your full name: ";
     getline(cin, name);
+
+    for (int m = 0; m < name.length(); m++)
+    {
+        if (name[m] == ' ')
+            count++;
+    }
     for (int i = 0; i < name.length(); i++)
     {
         if (name[i] == ' ')
             name.erase(name.find(' '), 1);
     }
-    cout << name << endl;
-    cout << "Your name has " << name.length() << " letters!" << endl;
+    cout << "Your full name has " << count << " names and " << name.length() << " letters!" << endl;
+
     return 0;
 }
