33fe6a9255627dc9e61dda751b96a69ead30d723
jaci-hub <jacintoquimua7@gmail.com>
2022-01-28 12:27:38 -0700

    done

--- CompetitiveProgrammingSolutions/validtictactoestate.cpp
+++ CompetitiveProgrammingSolutions/validtictactoestate.cpp
@@ -1,12 +1,9 @@
-#include <iostream>
-#include <string>
-#include<algorithm>
-#include<vector>
-using namespace std;
-
-bool validTicTacToe(vector<string>& board) {
-    int counterEmpty=0, counterX=0, counterO=0;
-    bool res=false;
+//https://leetcode.com/problems/valid-tic-tac-toe-state/
+class Solution {
+public:
+    bool validTicTacToe(vector<string>& board) {
+        
+        int counterEmpty=0, counterX=0, counterO=0;
         for(int i=0; i<3; i++){
             for(int j=0; j<3; j++){
                 if(board[i][j]==' ')
@@ -17,22 +14,70 @@ bool validTicTacToe(vector<string>& board) {
                     counterO++;
             }
         }
-        cout<<"*** "<<counterEmpty<<", "<<counterX<<", "<<counterO<<" ***\n";
         if(counterEmpty==9)
-            res= true;
+            return true;
         else if(counterEmpty==8 && counterX==1)
-            res= true;
-        else if(counterX - counterO == 1 || counterX - counterO == 0)
-            res= true;
-        
-        return res;
-}
-
-int main(){
-    vector<string> b={{'O', ' ', ' '},{' ', ' ', ' '},{' ', ' ', ' '}};
-    if(validTicTacToe(b))
-        cout<<"VALID!\n";
-    else cout<<"INVALID!\n";
-
-    return 0;
-}
+            return true;
+        else if(counterX-counterO==1){ // O shouldnt win
+            int finalCount=0, Oij1=0, Oij2=0, k=2;
+            for(int i=0; i<3; i++){
+                int Oj=0, Oi=0;
+                for(int j=0; j<3; j++){
+                    if(board[i][j]=='O')
+                        Oj++;
+                    if(board[j][i]=='O')
+                        Oi++;
+                }
+                if(board[i][i]=='O')
+                    Oij1++;
+                if(board[i][k]=='O'){
+                    Oij2++;
+                    k--;
+                }
+                
+                if(Oj!=3)
+                    finalCount++;
+                if(Oi!=3)
+                    finalCount++;
+            }
+            if(Oij1!=3)
+                finalCount++;
+            if(Oij2!=3)
+                finalCount++;
+            
+            if(finalCount==8)
+                return true;
+        }
+        else if(counterX-counterO==0){ // X shouldnt win
+            int finalCount=0, Xij1=0, Xij2=0, k=2;
+            for(int i=0; i<3; i++){
+                int Xj=0, Xi=0;
+                for(int j=0; j<3; j++){
+                    if(board[i][j]=='X')
+                        Xj++;
+                    if(board[j][i]=='X')
+                        Xi++;
+                }
+                if(board[i][i]=='X')
+                    Xij1++;
+                if(board[i][k]=='X'){
+                    Xij2++;
+                    k--;
+                }
+                
+                if(Xj!=3)
+                    finalCount++;
+                if(Xi!=3)
+                    finalCount++;
+            }
+            if(Xij1!=3)
+                finalCount++;
+            if(Xij2!=3)
+                finalCount++;
+            
+            if(finalCount==8)
+                return true;
+        }
+        return false;
+    }
+};
